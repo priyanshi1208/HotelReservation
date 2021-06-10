7a59784bda7b120badf7724ec79b9e27afae2253
priyanshi1208 <pri.shukla2208@gmail.com>
2021-06-10 13:32:20 +0530

    ADDED:validation of adding rewarded customers in the hotel list

--- src/test/java/com/magic/hotelreservation/HotelControlsTest.java
+++ src/test/java/com/magic/hotelreservation/HotelControlsTest.java
@@ -1,11 +1,11 @@
 package com.magic.hotelreservation;
 
 import com.magic.hotelreservation.entity.CustomerType;
-import com.magic.hotelreservation.entity.Hotel;
 import com.magic.hotelreservation.services.CrudHotel;
 import org.junit.Assert;
 import org.junit.Test;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class HotelControlsTest {
     @Test
@@ -17,4 +17,23 @@ public class HotelControlsTest {
         String Hotelname = crudHotel.cheapestRateAndHighestRated("11-09-2020", "12-09-2020");
         Assert.assertEquals(Optional.ofNullable("Lakewood"), Optional.ofNullable(Hotelname));
     }
+    @Test
+    public void validating_addition_of_special_rates_for_rewarded_customer() {
+        CrudHotel crudHotel = new CrudHotel();
+        crudHotel.addHotelForRegularCustomer("Lakewood",110,90,3, CustomerType.Regular_Customer);
+        crudHotel.addHotelForRegularCustomer("Bridgewood",160,60,4, CustomerType.Regular_Customer);
+        crudHotel.addHotelForRegularCustomer("Ridgewood",220,150,5, CustomerType.Regular_Customer);
+        crudHotel.addHotelForRegularCustomer("Lakewood",80,80,5, CustomerType.Rewarded_Customer);
+        crudHotel.addHotelForRegularCustomer("Bridgewood",110,50,5, CustomerType.Rewarded_Customer);
+        crudHotel.addHotelForRegularCustomer("Ridgewood",100,40,5, CustomerType.Rewarded_Customer);
+        AtomicReference<Integer> weekdaysRates = new AtomicReference<>(null);
+        crudHotel.hotelList.forEach(hotel->{
+            if(hotel.getCustomerType().equals(CustomerType.Rewarded_Customer)&&hotel.getHotelName().equals("Lakewood")){
+                 weekdaysRates.set(hotel.getWeekdaysRates());
+            }
+        });
+        Assert.assertEquals(80, Integer.parseInt(String.valueOf(weekdaysRates.get())));
+
+    }
+
 }
